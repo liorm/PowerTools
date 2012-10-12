ff41515efa034c555e0bc910a13fc690bc36e8da
Lior Mualem <lior@liormualem.com>
2012-10-12 19:56:28 +0200

    Improved globalization support.

--- Globalization/CultureManager.cs
+++ Globalization/CultureManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Reactive.Linq;
 using System.Threading;
 
 namespace LiorTech.PowerTools.Globalization
@@ -9,6 +10,12 @@ namespace LiorTech.PowerTools.Globalization
     /// </summary>
     public static class CultureManager
     {
+        static CultureManager()
+        {
+            ApplicationUICultureObservable = 
+                Observable.FromEvent<CultureChangedHandler, CultureInfo>(a_d => ApplicationUICultureChanged += a_d, a_d => ApplicationUICultureChanged -= a_d);
+        }
+
         /// <summary>
         /// Represents the method that will handle the <see cref="ApplicationUICultureChanged"/> event   
         /// </summary>
@@ -18,6 +25,21 @@ namespace LiorTech.PowerTools.Globalization
         /// Raised when the <see cref="ApplicationUICulture"/> is changed  
         /// </summary>
         public static event CultureChangedHandler ApplicationUICultureChanged;
+
+        /// <summary>
+        /// Returns an observable sequence that receives events when culture change occurs.
+        /// </summary>
+        public static IObservable<CultureInfo> ApplicationUICultureObservable { get; private set; }
+
+        /// <summary>
+        /// Return an observable that changes when the culture changes (e.g. selector could return a resource string).
+        /// </summary>
+        /// <param name="a_selector"></param>
+        /// <returns></returns>
+        public static IObservable<string> GetCultureString(Func<CultureInfo, string> a_selector)
+        {
+            return ApplicationUICultureObservable.StartWith(CultureManager.ApplicationUICulture).Select(a_selector);
+        }
 
         /// <summary>
         /// Set/Get the UICulture for whole application. 
---
+++ Globalization/ResourceCommandDescription.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Windows;
+using LiorTech.PowerTools.Commanding;
+
+namespace LiorTech.PowerTools.Globalization
+{
+    /// <summary>
+    /// Base class for a resource base <see cref="CommandDescriptionBase"/> instance.
+    /// </summary>
+    public abstract class ResourceCommandDescriptionBase : CommandDescriptionBase, IWeakEventListener
+    {
+        protected ResourceCommandDescriptionBase(string a_name, string a_commandImagePostfix = null) :
+            base(a_name)
+        {
+            CommandImagePostfix = a_commandImagePostfix;
+
+            ResetValues();
+
+            CultureChangedWeakEventManager.AddListener(this);
+        }
+
+        /// Returns the resources manager instance.
+        protected abstract ResourceManager ResourceManager { get; }
+
+        private void ResetValues()
+        {
+            ToolTip = ResourceManager.GetString(Name + "_tooltip") ?? string.Empty;
+            Text = ResourceManager.GetString(Name + "_text") ?? string.Empty;
+        }
+
+        #region Implementation of IWeakEventListener
+
+        public bool ReceiveWeakEvent(Type a_managerType, object sender, EventArgs e)
+        {
+            if (a_managerType != typeof(CultureChangedWeakEventManager))
+                return false;
+
+            ResetValues();
+
+            return true;
+        }
+
+        #endregion
+    }
+}
