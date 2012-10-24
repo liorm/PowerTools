02f52e015bc455a26eb0665292030d2c0b633d69
Lior Mualem <lior@liormualem.com>
2012-10-24 15:43:33 +0200

    Added dialogservices register behavior for datatemplates.

---
+++ ViewModel/RegisterViewBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace LiorTech.PowerTools.ViewModel
+{
+    /// <summary>
+    /// Set <see cref="DialogService.IsViewRegisteredProperty"/> automatically - it's useful for DataTemplates that instanciate dynamically.
+    /// </summary>
+    public class RegisterViewBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            DialogService.SetIsViewRegistered(AssociatedObject, true);
+        }
+
+        protected override void OnDetaching()
+        {
+            DialogService.SetIsViewRegistered(AssociatedObject, false);
+
+            base.OnDetaching();
+        }
+    }
+}
